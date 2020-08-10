fb8a0c4e1eee3a0f2e0ad0d177e6dcee4ce2e7ab
Tork_PC <tork.developer@tork.com>
2020-08-10 17:18:13 +0200

    Add support for linux

--- src/main/java/Task1/Task1.java
+++ src/main/java/Task1/Task1.java
@@ -2,23 +2,30 @@ package Task1;
 
 import java.awt.*;
 import java.io.File;
-import java.io.IOException;
 
 public class Task1 {
 
-    private static final String task1ResourcesDir = System.getProperty("user.dir") + "\\src\\main\\resources\\task1";
-
     public static void onOpeningResourceDirectory() {
+
+        String task1ResourcesDir;
+        if (System.getProperty("os.name").contains("Windows")) {
+            task1ResourcesDir = System.getProperty("user.dir") + "\\src\\main\\resources\\task1";
+        } else {
+            task1ResourcesDir = System.getProperty("user.dir") + "/src/main/resources/task1";
+        }
 
         System.out.println("Opening Task 1 Resource directory... \n" + task1ResourcesDir);
         System.out.println("\nThis directory has 3 folders. Each one contains a complete report for each bug");
 
         try {
             Desktop.getDesktop().open(new File(task1ResourcesDir));
-        } catch (IOException e) {
-            System.out.printf("The directory located at %s couldn't be opened", task1ResourcesDir);
+
+        } catch (Exception e) {
+            System.out.printf("The directory located at %s couldn't be opened\n", task1ResourcesDir);
+            e.printStackTrace();
         }
 
     }
 
 }
+
